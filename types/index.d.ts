36f1b8593f8bcf71bed62e26a257208031d84591
Noah Overcash <ncovercash@ua.edu>
2022-06-11 16:34:49 -0500

    Dump to self

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,137 +1,74 @@
 // lib/Accordion
-export const Accordion: any;
-export const AccordionSet: any;
-export const AccordionStatus: any;
-export const DefaultAccordionHeader: any;
-export const FilterAccordionHeader: any;
-export const ExpandAllButton: any;
-export const expandAllFunction: any;
 
 // lib/AdvancedSearch
-export const AdvancedSearch: any;
-export const defaultAdvancedSearchQueryBuilder: any;
-export const ADVANCED_SEARCH_BOOLEAN_OPERATORS: any;
 
 // lib/AutoSuggest
-export const AutoSuggest: any;
 
 // lib/Avatar
-export const Avatar: any;
 
 // lib/Badge
 export { default as Badge } from "./lib/Badge";
 
 // lib/Button
-export const Button: any;
 
 // lib/ButtonGroup
-export const ButtonGroup: any;
 
 // lib/Callout
-export const Callout: any;
-export const CalloutElement: any;
 
 // lib/Card
-export const Card: any;
 
 // lib/Checkbox
-export const Checkbox: any;
 
 // lib/Commander
-export const CommandList: any;
-export const HasCommand: any;
-export const expandAllSections: any;
-export const collapseAllSections: any;
-export const checkScope: any;
-export const defaultKeyboardShortcuts: any;
-export const keyboardShortcutNames: any;
-export const importShortcuts: any;
-export const renameShortcutLabels: any;
 
 // lib/ConfirmationModal
-export const ConfirmationModal: any;
 
 // lib/ConflictDetectionBanner
-export const ConflictDetectionBanner: any;
 
 // lib/CountrySelection
-export const CountrySelection: any;
 
 // lib/CurrencySelect
-export const CurrencySelect: any;
 
 // lib/Datepicker
-export const Datepicker: any;
-export const Calendar: any;
 
 // lib/DateRangeWrapper
-export const DateRangeWrapper: any;
 
 // lib/Dropdown
-export const Dropdown: any;
-export const UncontrolledDropdown: any; // lib/Dropdown;
 
 // lib/DropdownButton
-export const DropdownButton: any;
 
 // lib/DropdownMenu
-export const DropdownMenu: any;
 
 // lib/Editor
-export const Editor: any;
 
 // lib/EmptyMessage
-export const EmptyMessage: any;
 
 // lib/ErrorBoundary
-export const ErrorBoundary: any;
 
 // lib/ErrorModal
-export const ErrorModal: any;
 
 // lib/ExportCsv
-export const ExportCsv: any;
-export const exportToCsv: any;
 
 // lib/FilterControlGroup
-export const FilterControlGroup: any;
 
 // lib/FilterGroups
-export const FilterGroups: any;
-export const filterState: any;
-export const filters2cql: any;
-export const handleClearAllFilters: any;
-export const handleFilterChange: any;
-export const handleFilterClear: any;
-export const initialFilterState: any;
-export const onChangeFilter: any;
-export const FILTER_SEPARATOR: any;
-export const FILTER_GROUP_SEPARATOR: any;
 
 // lib/FilterPaneSearch
-export const FilterPaneSearch: any;
 
 // lib/FocusLink
-export const FocusLink: any;
 
 // lib/FormattedDate
-export const FormattedDate: any;
 
 // lib/FormattedTime
-export const FormattedTime: any;
 
 // lib/FormattedUTCDate
-export const FormattedUTCDate: any;
 
 // lib/Headline
 export { default as Headline } from "./lib/Headline";
 
 // lib/Highlighter
-export const Highlighter: any;
 
 // lib/HotKeys
-export const HotKeys: any;
-export const FocusTrap: any;
 
 // lib/Icon
 export { default as Icon } from "./lib/Icon";
@@ -140,199 +77,200 @@ export { default as Icon } from "./lib/Icon";
 export { default as IconButton } from "./lib/IconButton";
 
 // lib/InfoPopover
-export const InfoPopover: any;
 
 // lib/KeyboardShortcutsModal
-export const KeyboardShortcutsModal: any;
 
 // lib/KeyValue
-export const KeyValue: any;
 
 // lib/Label
-export const Label: any;
 
 // lib/Layer
-export const Layer: any;
 
 // lib/Layout
-export const Layout: any;
 
 // lib/LayoutBox
-export const LayoutBox: any;
 
 // lib/LayoutGrid
-export const Grid: any;
-export const Row: any;
-export const Col: any;
 
 // lib/LayoutHeader
-export const LayoutHeader: any;
 
 // lib/List
-export const List: any;
 
 // lib/Loading
-export const Loading: any;
-export const LoadingPane: any;
-export const LoadingView: any;
 
 // lib/MenuItem
-export const MenuItem: any;
 
 // lib/MenuSection
-export const MenuSection: any;
 
 // lib/MessageBanner
-export const MessageBanner: any;
 
 // lib/MetaSection
-export const MetaSection: any;
 
 // lib/Modal
-export const Modal: any;
 
 // lib/ModalFooter
-export const ModalFooter: any;
 
 // lib/MultiColumnList
-export const MultiColumnList: any;
-export const DefaultMCLRowFormatter: any;
-export const MCLPagingTypes: any;
-export const EndOfList: any;
 
 // lib/MultiSelection
-export const MultiSelection: any;
 
 // lib/NavList
-export const NavList: any;
 
 // lib/NavListItem
-export const NavListItem: any;
 
 // lib/NavListSection
-export const NavListSection: any;
 
 // lib/NoValue
-export const NoValue: any;
 
 // lib/Pane
-export const Pane: any;
 
 // lib/PaneBackLink
-export const PaneBackLink: any;
 
 // lib/PaneCloseLink
-export const PaneCloseLink: any;
 
 // lib/PaneFooter
-export const PaneFooter: any;
 
 // lib/PaneHeader
-export const PaneHeader: any;
 
 // lib/PaneHeaderIconButton
 export { default as PaneHeaderIconButton } from "./lib/PaneHeaderIconButton";
 
 // lib/PaneMenu
-export const PaneMenu: any;
 
 // lib/Paneset
-export const Paneset: any;
 
 // lib/PaneSubheader
-export const PaneSubheader: any;
 
 // lib/PasswordStrength
-export const PasswordStrength: any;
 
 // lib/Popover
-export const Popover: any;
 
 // lib/Popper
-export const Popper: any;
-export const AVAILABLE_PLACEMENTS: any; // lib/Popper
 
 // lib/RadioButton
-export const RadioButton: any;
 
 // lib/RadioButtonGroup
-export const RadioButtonGroup: any;
 
 // lib/RepeatableField
-export const RepeatableField: any;
 
 // lib/SearchField
-export const SearchField: any;
 
 // lib/Select
-export const Select: any;
 
 // lib/Selection
-export const Selection: any;
-export const OptionSegment: any;
 
 // lib/Spinner
-export const Spinner: any;
 
 // lib/SRStatus
-export const SRStatus: any;
 
 // lib/TextArea
-export const TextArea: any;
 
 // lib/TextField
-export const TextField: any;
 
 // lib/TextLink
-export const TextLink: any;
 
 // lib/Timepicker
-export const Timepicker: any;
 
 // lib/Tooltip
-export const Tooltip: any;
 
 // hooks/useCurrencyOptions
-export const useCurrencyOptions: any;
 
 // hooks/useFormatDate
-export const useDateFormatter: any;
 
 // hooks/useFormatTime
-export const useTimeFormatter: any;
 
 // util/countries
-export const countries: any;
-export const countriesByCode: any;
-export const countryCodes: any;
 
 // util/currencies
-export const currencies: any;
-export const currenciesByCode: any;
-export const currenciesByName: any;
-export const currenciesByNumber: any;
-export const currenciesOptions: any;
 
 // util/dateTimeUtils
-export const getLocaleDateFormat: any;
-export const getLocalizedTimeFormatInfo: any;
 
 // util/getFocusableElements
-export const getNextFocusable: any;
-export const getPreviousFocusable: any;
-export const getLastFocusable: any;
-export const getFirstFocusable: any;
 
 // util/languages
-export const formattedLanguageName: any;
-export const languageOptions: any;
-export const languages: any;
 
 // util/nativeChangeFieldValue
-export const nativeChangeFieldValue: any;
 
 // util/omitProps
-export const omitProps: any;
 
 // util/RootCloseWrapper
-export const RootCloseWrapper: any;
+
+import {
+  Component,
+  CSSProperties,
+  ElementType,
+  FunctionComponent,
+  ReactNode,
+} from "react";
+import { FieldInputProps, FieldMetaState } from "react-final-form";
+
+// either children or innerText must be provided
+export const OptionSegment: FunctionComponent<
+  {
+    searchTerm?: string;
+  } & ({ children: ReactNode } | { innerText: string })
+>;
+export class Loading extends Component<{}> {}
+export class Pane extends Component<{
+  actionMenu?: Function;
+  actionMenuAutoFocus?: boolean;
+  appIcon?: ReactNode; // TODO: explicitly require an <AppIcon> once stripes-core specifies typings
+  centerContent?: boolean;
+  children?: ReactNode;
+  defaultWidth: `${number}%` | "fill";
+  dismissible?: boolean | "last";
+  firstMenu?: ReactNode;
+  fluidContentWidth?: boolean;
+  footer?: ReactNode;
+  height?: string;
+  id?: string;
+  lastMenu?: ReactNode;
+  noOverflow?: boolean;
+  onClose?: Function;
+  onMount?: Function;
+  padContent?: boolean;
+  paneSub?: ReactNode;
+  paneTitle?: ReactNode;
+  paneTitleRef?: ReactNode;
+  renderHeader?: Function;
+  subheader?: ReactNode;
+  tagName?: ElementType;
+  transition?: string;
+}> {}
+
+export class MultiSelection<OptionType = { label: string }> extends Component<{
+  input: FieldInputProps<OptionType[], HTMLElement>;
+  meta: FieldMetaState<OptionType[]>;
+
+  actions?: { onSelect: Function }[];
+  ariaLabelledBy?: string;
+  asyncFiltering?: boolean;
+  autoFocus?: boolean;
+  backspaceDeletes?: boolean;
+  dataOptions?: OptionType[];
+  dirty?: boolean;
+  disabled?: boolean;
+  emptyMessage?: string;
+  error?: ReactNode;
+  filter?: (
+    filterText: string | undefined,
+    list: OptionType[]
+  ) => { renderedItems: OptionType[]; exactMatch?: boolean };
+  formatter?: (option: OptionType, searchTerm: string | undefined) => ReactNode;
+  id?: string;
+  isValid?: boolean;
+  itemToString?: (option: OptionType) => string;
+  label?: ReactNode;
+  maxHeight?: number;
+  modifiers?: object;
+  onBlur?: Function;
+  onChange?: Function;
+  onRemove?: Function;
+  placeholder?: string;
+  renderToOverlay?: boolean;
+  required?: boolean;
+  validationEnabled?: boolean;
+  value?: OptionType[];
+  valueFormatter?: (option: OptionType) => ReactNode;
+  warning?: ReactNode;
+}> {}
